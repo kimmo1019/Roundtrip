b7de21d2e19d65c3bbbe1a9d8215a592ec7592e8
kimmo1019 <liuqiao@buaa.edu.cn>
2023-04-13 14:12:43 -0700

    add simulation examples

--- src/pyroundtrip/__init__.py
+++ src/pyroundtrip/__init__.py
@@ -1,3 +1,3 @@
 __version__ = '2.0.0'
 from .roundtrip import Roundtrip, VariationalRoundtrip, RoundtripTV, RoundtripTV_img
-from .util import Base_sampler, Outlier_sampler, UCI_sampler, hepmass_sampler, mnist_sampler, cifar10_sampler
+from .util import Base_sampler, Outlier_sampler, UCI_sampler, mnist_sampler, cifar10_sampler,GMM_indep_sampler,GMM_sampler,Swiss_roll_sampler
